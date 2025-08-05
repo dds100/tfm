8c4b99c215539d287bbdb0408c31df92b7d34c46
dds100 <dds101@alumnos.unican.es>
2025-08-05 11:09:19 +0200

    Detecta flanco correctamente. Se salta algunos elementos por luz o alto tiempo de exposición.

--- main2.py
+++ main2.py
@@ -24,7 +24,7 @@ image_shape = frame.shape
 # Inicialización de variables
 detections = []
 already_crossed = []
-y_margin = 15
+y_margin = 25
 x_margin = 15
 
 while True:
@@ -45,36 +45,45 @@ while True:
     annotated_frame = results[0].plot()
     cycle_time = time.time() - inicio
     cv2.putText(annotated_frame, f'FPS:{1/cycle_time:.2f}', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
-    # print(f'FPS: {1/cycle_time:.2f}')
 
     # Dibujar línea arbitraria de cruce
     signal_line = 120
     cv2.line(annotated_frame, (0, signal_line), (image_shape[1], signal_line), (0, 0, 255), 2)
 
     # Generar una lista con clase y centro (o punto)
+    crossing_detections = []
     for box in results[0].boxes:
         cx, cy = box.xywh[0][:2]
         class_name = results[0].names[int(box.cls[0])]
-        detections.append({'cx': cx, 'cy': cy, 'class_name': class_name})
-
+        detection = {'cx': cx, 'cy': cy, 'class_name': class_name}
 
         is_crossing_line = abs(cy - signal_line) < y_margin
-        is_in_already_clossed = any([abs(cx-cx_crossed) < x_margin for cx_crossed in already_crossed])
-        if is_crossing_line and not is_in_already_clossed:
+        is_in_already_crossed = any([abs(cx-cx_crossed) < x_margin for cx_crossed in already_crossed])
+        if is_crossing_line:
             color = (0, 255, 0)
-            already_crossed.append(cx)
-            signal = True
-            print('Ha cruzado pipa o pipa pelada.')
+            crossing_detections.append(detection)
+            already_crossed.append(int(cx))
+            if not is_in_already_crossed:
+                signal = True
+                print(f'Ha cruzado {detection["class_name"]}.')
         else:
             color = (0, 0, 255)
-        # cv2.circle(annotated_frame, (int(cx), int(cy)), 3, color, -1)
         cv2.line(annotated_frame, (int(cx), int(cy-y_margin)), (int(cx), int(cy+y_margin)), color, 2)
         
-        # # Eliminar las que ya no estén cruzando
-        # for x in already_crossed:
-        #     for detection in detections:
-        #         if 
-    
+    # Pintar de más oscuro las zonas ya cruzadas
+    for x in already_crossed:
+        cv2.line(annotated_frame, (x-x_margin, signal_line), (x+x_margin, signal_line), (0, 0, 100), 2)
+
+    # Eliminar las que ya no estén cruzando
+    new_already_crossed = []
+    for x_coord in already_crossed:
+        crossing_now = False
+        for detection in crossing_detections:
+            if abs(detection['cx']-x_coord) < x_margin:
+                crossing_now = True
+        if crossing_now:
+            new_already_crossed.append(x_coord)
+    already_crossed = new_already_crossed
         
 
     # Activar señal de pipa cruzando
